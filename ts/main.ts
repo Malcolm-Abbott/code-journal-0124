5422dae9bab36f6172a046aaf5ef0747f52a97f3
Malcolm Abbott <mjacoding@gmail.com>
2024-02-19 21:01:50 +0000

    Add input event listener to Photo url && add submit event listener to form

--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,38 @@
 /* global data */
+const $photo = document.querySelector('#photo') as HTMLInputElement;
+const $title = document.querySelector('#title') as HTMLInputElement;
+const $notes = document.querySelector('#notes') as HTMLTextAreaElement;
+const $imgNew = document.querySelector('.img-new') as HTMLImageElement;
+
+if (!$title) throw new Error('The $title query failed');
+if (!$notes) throw new Error('The $notes query failed');
+
+$photo?.addEventListener('input', (): void => {
+  $imgNew?.setAttribute('src', $photo.value);
+});
+
+const $form = document.querySelector('form');
+
+$form?.addEventListener('submit', (event: Event) => {
+  event.preventDefault();
+
+  interface Values {
+    title: string;
+    photo: string;
+    notes: string;
+    entryId?: number;
+  }
+
+  const title = $title.value;
+  const photo = $photo.value;
+  const notes = $notes.value;
+
+  const values: Values = {
+    title,
+    photo,
+    notes,
+  };
+
+  values.entryId = data.nextEntryId;
+  data.nextEntryId++;
+});
