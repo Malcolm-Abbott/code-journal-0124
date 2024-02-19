3a0e3d4cd502a42bb7fe68023b6cb6c01556a211
Malcolm Abbott <mjacoding@gmail.com>
2024-02-19 23:37:41 +0000

    Listen for the DOMContentLoaded event and render a DOM tree for each entry in the data.entries arrays stores in the data model

--- ts/main.ts
+++ ts/main.ts
@@ -39,3 +39,44 @@ $form?.addEventListener('submit', (event: Event) => {
   $imgNew.setAttribute('src', '../images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+function renderEntry(entry: Values): HTMLLIElement {
+  const $li = document.createElement('li') as HTMLLIElement;
+
+  const $row = document.createElement('div') as HTMLDivElement;
+  $row.className = 'row';
+  $li.append($row);
+
+  const $colHalf = document.createElement('div') as HTMLDivElement;
+  $colHalf.className = 'column-half';
+  $row.append($colHalf);
+
+  const $imgEntriesWrapper = document.createElement('div') as HTMLDivElement;
+  $imgEntriesWrapper.className = 'img-entries-wrapper';
+  $colHalf.append($imgEntriesWrapper);
+
+  const $img = document.createElement('img') as HTMLImageElement;
+  $img.setAttribute('src', entry.photo);
+  $imgEntriesWrapper.append($img);
+
+  const $colHalf2 = document.createElement('div') as HTMLDivElement;
+  $colHalf2.className = 'column-half';
+  $row.append($colHalf2);
+
+  const $h3 = document.createElement('h3') as HTMLHeadingElement;
+  $h3.textContent = entry.title;
+  $colHalf2.append($h3);
+
+  const $p = document.createElement('p') as HTMLParagraphElement;
+  $p.textContent = entry.notes;
+  $colHalf2.append($p);
+
+  return $li;
+}
+
+document.addEventListener('DOMContentLoaded', (): void => {
+  const $ul = document.querySelector('ul') as HTMLUListElement;
+  data.entries.forEach((entry) => {
+    $ul.prepend(renderEntry(entry));
+  });
+});
